b2b5ae48a1e970231faab399753e55f4ca3916b9
shah <shahqa98@gmail.com>
2026-02-03 00:47:47 +0500

    Update forgotPassword.cy.js

--- cypress/e2e/forgotPassword/forgotPassword.cy.js
+++ cypress/e2e/forgotPassword/forgotPassword.cy.js
@@ -20,4 +20,38 @@ describe('Forgot Password', () => {
 
     loginPage.verifyToastMessage("We send a recovery mail. Please follow the instructions to recover your password.");  
   });
+
+  it('Forgot password with unregistered email', () => {
+
+    // 🌐 Visit the app's root page
+    cy.visit('/');
+    loginPage.verifyLandingBody();
+
+    //Navigate to login page
+    loginPage.clickProfileIcon();
+
+    //Click Forgot Password button
+    loginPage.forgotPasswordButton();
+
+    forgotPasswordModal('shah@gmail.com');
+
+    loginPage.verifyToastMessage("Email not found");  
+  });
+
+  it.only('Forgot password with invalid email', () => {
+
+    // 🌐 Visit the app's root page
+    cy.visit('/');
+    loginPage.verifyLandingBody();
+
+    //Navigate to login page
+    loginPage.clickProfileIcon();
+
+    //Click Forgot Password button
+    loginPage.forgotPasswordButton();
+
+    forgotPasswordModal('shah.com');
+
+    // loginPage.verifyToastMessage("Email not found");  
+  });
 });
